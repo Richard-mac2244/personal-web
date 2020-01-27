705023689ab98ea43b9c7ef5d1de4bcc99a5bcaa
Richard Mac <richard.mac2244@gmail.com>
2020-01-26 22:39:17 -0800

    Edit info

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -11,8 +11,9 @@ function MainContainer({ title, subtitle, dark, id }) {
         </div>
         <div className="body">
           <p className= "about_me"> Hi, my name is Richard Mac and welcome to my personal website. I am a senior attending San Jose State
-            University and transitioning over to software engineering field. I love taking on challenges and creating
-            solutions that make everyone's lives a lot easier. Well what are you waiting for, take a look around!
+            University and transitioning over to software engineering field. I realized throughout my college career was that I love learning new
+            things and pushing myself to expand my skillset. I love taking on new challenges and working on new technologies that have a direct impact on
+            everyone's lives. This is a personal website to showcase some of my projects and social media platforms that I am usually on!
           </p>
         </div>
       </div>
