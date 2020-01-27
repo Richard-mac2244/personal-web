99afc240708033f1a163ee61b445712e77c3ec4d
Richard Mac <richard.mac2244@gmail.com>
2020-01-26 22:28:38 -0800

    Debug

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -12,11 +12,9 @@ function Projects({ title, dark, id }) {
       <div className="section-content" id={id}>
         <h1>{title}</h1>
         <div className="section-project">
-          <FadeInSection >
-            <img className="proja1" src = {proja1} alt="Project_Image_1"/>
-          </FadeInSection>
           {bodies.map(body => (
             <FadeInSection key = {body}>
+              <img className="proja1" src = {proja1} alt="Project_Image_1"/>
               <p className={body.substring(0,6)}> {body.substring(7)} </p>
             </FadeInSection>
           ))}
